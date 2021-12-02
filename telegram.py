d38ce874c13aabfc9000ff35360e3600ff0af84a
daviFrag <73591373+daviFrag@users.noreply.github.com>
2021-12-02 12:10:32 +0100

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -46,7 +46,7 @@ def newsToMsg(obj : News) -> str:
     #     case "ATENEO":
     #         return  '<b>ATENEO news</> \n' + content.title + '\n' + content.link + '\n' + content.published
     if obj.category=="DMATH":
-        return '<b>#NEWS</> \n' + content.title + '\n' + content.link + '\n' + content.published
+        return '#NEWS \n' + content.title + '\n' + content.link + '\n' + content.published
     elif obj.category=="ATENEO":
         return '<b>ATENEO</> \n' + content.title + '\n' + content.link + '\n' + content.published
     return ""
